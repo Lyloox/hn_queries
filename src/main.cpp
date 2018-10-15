720e95a6c876e643db24ad20846af35ce9f79e21
Laure Daumal <daumal_l@epita.fr>
2018-10-15 23:49:42 +0200

    Update parsing: using map as counter dict

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,44 @@
 #include <sstream>
 #include <vector>
 #include <iterator>
+#include <map>
+#include <set>
 
 using namespace std;
 
 class BinaryTree {
   public:
+
     class LogObject {
+
       public:
         unsigned int timestamp;
-        string query;
+        unsigned int nb_query;
+        vector<string> queries;
         LogObject(unsigned int ts, string q) : timestamp(ts),
-                                               query(q) {}
+                                               nb_query(1),
+                                               queries(1, q) {}
+
+        void add(string& query) {
+          nb_query++;
+          queries.push_back(query);
+        }
+
+        friend ostream& operator<<(ostream& os, LogObject& log);
         friend class BinaryTree;
     };
 
   protected:
-    vector<LogObject> logs;
+    map<unsigned int, LogObject> logs;
+
+    friend ostream& operator<<(ostream& os, LogObject& log) {
+      os << log.nb_query << " queries: ";
+      for (vector<string>::iterator query = log.queries.begin(); 
+          query != log.queries.end(); ++query) {
+        os << *query << " ";
+      }
+      return os;
+    }
 
   public:
     BinaryTree() {
@@ -28,22 +50,27 @@ class BinaryTree {
       return int(logs.size()) - 1;
     }
 
-    void insert(const LogObject& log) {
-      logs.push_back(log);
+    void insert(unsigned int key, string value) {
+      auto previous_log = logs.find(key);
+      if (previous_log != logs.end()) {
+        previous_log->second.add(value);
+      }
+      else {
+        LogObject log(key, value);
+        logs.insert({log.timestamp, log});
+      }
     }
 
     void print() {
-      for (vector<LogObject>::iterator log = logs.begin();
+      for (map<unsigned int, LogObject>::iterator log = logs.begin();
           log != logs.end(); ++log) {
-        cout << log->timestamp << "; " << log->query << endl;
+        cout << log->first << "; " << log->second << endl;
       }
     }
 };
 
 int main()
 {
-  typedef BinaryTree::LogObject LogObject;
-
   ifstream logs_file("logs.txt");
   BinaryTree tree;
   if (logs_file.is_open()) {
@@ -54,8 +81,7 @@ int main()
       string value;
       iss >> key;
       iss >> value;
-      LogObject log(key, value);
-      tree.insert(log);
+      tree.insert(key, value);
     }
     logs_file.close();
   }
